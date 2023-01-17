b41dee154a868abee5b656c386cfd2e8a7b54fc3
“Meike <m.e.j.span@students.uu.nl>
2023-01-17 15:47:55 +0100

    made more efficient by fitting monthly in one loop

--- weather_assignment.py
+++ weather_assignment.py
@@ -6,7 +6,7 @@ from csv import DictReader
 #first, we need to set everything up:
 
 
-#opening our measurements and loading it into a list of dictionaries with each item being measurement and its information
+#opening our measurements and loading it into a list of dictionaries with each item being a measurement and its information
 with open('precipitation.json') as f:
     measurements = json.load(f)
 
@@ -15,23 +15,22 @@ with open('stations.csv') as csvfile:
     stationsfile = DictReader(csvfile)
     stations = list(stationsfile)
 
-#creating an empty directory in which we can later store all our data
+#creating an empty directory in which we can later store all our results
 results = {}
 
-#calculating the total precipitation across all stations, so the sum off all measurements
+#calculating the total precipitation across all stations, so the sum off all measurements, so we can use it later:
 total_yearly_precipitation_across_all_stations = sum(measurement['value'] for measurement in measurements)
 
-sum_relative_yearly_precipitation = 0
 
 ##-------------------------------------------------------------------------------------------------------------
 ##-------------------------------------------------------------------------------------------------------------
 #now, we can start calculating our informative datapoints:
 
 
-# creating a loop that calculates everything for each station
+# creating a loop that calculates everything for each station seperately
 for station in stations:
 
-#getting the keys we need to get the measurements and information for each station
+#getting the keys we need to get the measurements and information for our current specific station
     station_name = station['Location']
     station_state = station['State']
     station_code = station['Station']
@@ -42,19 +41,16 @@ for station in stations:
 
     total_monthly_precipitation = [0]*12 #creating a list in which we will collect our data, starting at 0 for each month
 
-    for measurement in measurements:  # for each measurment in our file, we are going to evaluate:
-        if measurement['station'] == station_code: # wether it is a measurement at the station we are currently working on
-
-            #in which month it was collected, after which we add it to its index in our total month list
-            #from looking at our data, i know all of our data was collected in the year 2010, so each date will start with 2010-month
-            for i in range(1,10): #for our single-digit months, so january - september
-                if  measurement['date'].startswith(f'2010-0{i}'): #if that measurement is in that month
-                    total_monthly_precipitation[i-1] += measurement['value'] #add the value up to the corresponding item in our list
-
-            for i in range(10,13): #for october - december
-                if  measurement['date'].startswith(f'2010-{i}'): #if that measurement is in that month
-                    total_monthly_precipitation[i-1] += measurement['value'] #add the value up to the corresponding item in our list
-
+    for measurement in measurements:  # for each measurement in our file, we are going to evaluate:
+        if measurement['station'] == station_code: # wether it is a measurement at the station we are currently working on, and if it is:
+            
+            #in which month it was collected, after which we add it to the sum at the corresponding index in our total month list
+            for i in range(1,13): #for each month seperately, we are telling it to
+                if i < 10:  #for january-september, make sure the month is denoted by two numbers, 01, 02 etc. instead of 1, 2
+                    i = f'0{i}'
+                if  measurement['date'].startswith(f'{i}', 5): #if the date for each measurement has the month code we are looking for in this loop on position 6 (as each date starts with 2010-):
+                    total_monthly_precipitation[int(i)-1] += measurement['value'] #add the value to our month total in the correct index of our list for this month
+                    
 #--------------------------------------------------------------------------------------------------------
     #calculating the total yearly precipation
     total_yearly_precipitation = sum(total_monthly_precipitation)
@@ -63,8 +59,8 @@ for station in stations:
     # calculating the relative monthly precipation
     relative_monthly_precipitation = [0]*12
 
-    for i in range(1,13):
-        relative_monthly_precipitation[i-1] = total_monthly_precipitation[i-1]/total_yearly_precipitation
+    for i in range(1,13): # for month 1-12
+        relative_monthly_precipitation[i-1] = total_monthly_precipitation[i-1]/total_yearly_precipitation #calculate the relative precipation per that month
 
 #----------------------------------------------------------------------------------------------------------
     # calculating the relative yearly precipation
